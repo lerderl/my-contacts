c8e0ef60a6ec1edcaa46471142ef389f30d72c45
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-17 11:38:29 +0100

    Composing with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+const ContactList = (props) =>  {
+  const people = props.contacts;
+
+  return (
+    <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={[ { name: 'Goodnews' }, { name: 'Egho' }, { name: 'Ozioma' } ]} />
+      <ContactList contacts={[ { name: 'Alvin' }, { name: 'Beauty' }, { name: 'Samuel' } ]} />
     </div>
   );
 }
