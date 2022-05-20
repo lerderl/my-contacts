87df8105205938dde5364ca11697d88c1e0ee857
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-20 09:06:32 +0100

    Added a search field

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,19 +1,38 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const ListContacts = ({ contacts, onDeleteContact }) => {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  }
+
   return (
-    <ol className="contact-list">
-      {contacts.map((contact) => (
-        <li key={contact.id} className="contact-list-item">
-          <div className="contact-avatar" style={{backgroundImage: contact.avatarURL}}></div>
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-          </div>
-          <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
-        </li>
-      ))}
-    </ol>
+    <div className='list-contacts'>
+      <div className='list-contacts-top'>
+        <input
+          className='search-contacts'
+          type={"text"}
+          placeholder="Search Contacts"
+          value={query}
+          onChange={(event) => updateQuery(event.target.value)}
+        />
+      </div>
+      <ol className="contact-list">
+        {console.log(query)}
+        {contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div className="contact-avatar" style={{backgroundImage: contact.avatarURL}}></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
