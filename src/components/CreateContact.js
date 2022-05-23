80968efa2cc68463ed1d6df8edfb129a25ecb995
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-23 15:30:13 +0100

    Added serializer for the for data

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
+import serializeForm from "form-serialize";
 
 import ImageInput from "./ImageInput";
 
-const CreateContact = () => {
+const CreateContact = ({ onCreateContact }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const values = serializeForm(e.target, { hash: true });
+
+    if (onCreateContact) {
+      onCreateContact(values);
+    }
+  }
+
   return (
     <div>
       <Link className="close-create-contact" to="/">Close</Link>
-      <form className="create-contact-form">
+      <form onSubmit={handleSubmit} className="create-contact-form">
         <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64} />
         <div className="create-contact-details">
           <input type="text" name="name" placeholder="Name" />
